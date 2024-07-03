eb384d630adb5ca5491c5af302d7448b2c5ad868
iamzainn <iamzainnf@gmail.com>
2024-07-03 16:51:42 +0500

    add message functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ export default function Home() {
   return (
     <main className='m-5'>
     <div className='flex overflow-y-hidden h-[calc(100vh-50px)] max-w-[1700px] mx-auto bg-left-panel'>
-  
       <div className='fixed top-0 left-0 w-full h-36 bg-green-primary dark:bg-transparent -z-30' />
       <LeftPanel />
       <RightPanel />
